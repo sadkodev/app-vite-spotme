c485b18a0c230de50455fb905f2b3f10c0af3419
Kristian Kastillo <kristiankastillo0@gmail.com>
2022-05-23 11:46:41 -0400

    init: git

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <router-view></router-view>
+</template>
+<script>
+export default {
+  name: 'App',
+}
+</script>
+<style>
+html {
+  background-color: #101016;
+  color: #c7c7f2;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <article v-for="product in products">
+      <h1>{{ product.title }}</h1>
+      <p>{{ product.description }}</p>
+      <p>price : {{ product.price }}</p>
+    </article>
+  </div>
+</template>
+
+<script>
+import { useProductStore } from '../stores/ProductsStore'
+
+export default {
+  name: 'Home',
+  data: () => {
+    return { products: useProductStore().products }
+  },
+}
+</script>
+<style scoped></style>
